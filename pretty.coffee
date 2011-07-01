b34aed31ebd7b71d7132b57ff3b74473bbe0d8f3
Tavish Armstrong <tavisharmstrong@gmail.com>
2011-07-01 15:12:26 -0400

    Initial commit.

---
+++ pretty.coffee
@@ -0,0 +1,74 @@
+# write_pretty_story, a writing program for writers
+# who don't want to edit what they write.
+
+filterText = (text) ->
+  filteredText = text.replace('--', '&mdash;')
+
+  # split on empty line (markdown style)
+  paragraphs = filteredText.split(/\n\n\n*/)
+  return paragraphs
+
+formatNewParagraph = (text) ->
+  paragraphs = filterText(text)
+  for paragraph in paragraphs
+    newParagraph = $(document.createElement('p'))
+    newParagraph.html(paragraph)
+    $('#written-text').append(newParagraph)
+
+makeNewParagraph = () ->
+  textarea = $('#writebox textarea')
+  text = textarea.val()
+  formatNewParagraph(text)
+  textarea.val('')
+
+onkeypress = (event) ->
+  if event.which is 13
+    $('#write-form').submit()
+
+onSubmit = (event) ->
+  event.preventDefault()
+  makeNewParagraph()
+
+mainText =
+  textArea: () ->
+    $('#writebox textarea')
+  appStuff: () ->
+    $('h1.story-title,#bd')
+  show: () ->
+    @appStuff().show()
+    @textArea().focus()
+  hide: () ->
+    @appStuff().hide()
+
+titleTextBox = () ->
+  $('#title-edit input[name="title"]')
+
+showTitleRequest = () ->
+  $('#title-edit').addClass('requesting')
+  titleTextBox().focus()
+
+hideTitleRequest = () ->
+  $('#title-edit').removeClass('requesting')
+
+getTitle = () ->
+  titleInput = titleTextBox()
+  title = titleInput.val()
+  $('h1.story-title').html(title)
+  hideTitleRequest()
+  mainText.show()
+
+requestTitle = () ->
+  mainText.hide()
+  showTitleRequest()
+
+onReady = () ->
+  console.log('ready')
+  $('#write-form').submit(onSubmit)
+  $('#writebox textarea').keypress(onkeypress)
+  requestTitle()
+  titleTextBox().keypress(() ->
+    if event.which is 13
+      getTitle()
+  )
+
+$(document).ready(onReady)
---
+++ pretty.js
@@ -0,0 +1,86 @@
+(function() {
+  var filterText, formatNewParagraph, getTitle, hideTitleRequest, mainText, makeNewParagraph, onReady, onSubmit, onkeypress, requestTitle, showTitleRequest, titleTextBox;
+  filterText = function(text) {
+    var filteredText, paragraphs;
+    filteredText = text.replace('--', '&mdash;');
+    paragraphs = filteredText.split(/\n\n\n*/);
+    return paragraphs;
+  };
+  formatNewParagraph = function(text) {
+    var newParagraph, paragraph, paragraphs, _i, _len, _results;
+    paragraphs = filterText(text);
+    _results = [];
+    for (_i = 0, _len = paragraphs.length; _i < _len; _i++) {
+      paragraph = paragraphs[_i];
+      newParagraph = $(document.createElement('p'));
+      newParagraph.html(paragraph);
+      _results.push($('#written-text').append(newParagraph));
+    }
+    return _results;
+  };
+  makeNewParagraph = function() {
+    var text, textarea;
+    textarea = $('#writebox textarea');
+    text = textarea.val();
+    formatNewParagraph(text);
+    return textarea.val('');
+  };
+  onkeypress = function(event) {
+    if (event.which === 13) {
+      return $('#write-form').submit();
+    }
+  };
+  onSubmit = function(event) {
+    event.preventDefault();
+    return makeNewParagraph();
+  };
+  mainText = {
+    textArea: function() {
+      return $('#writebox textarea');
+    },
+    appStuff: function() {
+      return $('h1.story-title,#bd');
+    },
+    show: function() {
+      this.appStuff().show();
+      return this.textArea().focus();
+    },
+    hide: function() {
+      return this.appStuff().hide();
+    }
+  };
+  titleTextBox = function() {
+    return $('#title-edit input[name="title"]');
+  };
+  showTitleRequest = function() {
+    $('#title-edit').addClass('requesting');
+    return titleTextBox().focus();
+  };
+  hideTitleRequest = function() {
+    return $('#title-edit').removeClass('requesting');
+  };
+  getTitle = function() {
+    var title, titleInput;
+    titleInput = titleTextBox();
+    title = titleInput.val();
+    $('h1.story-title').html(title);
+    hideTitleRequest();
+    return mainText.show();
+  };
+  requestTitle = function() {
+    mainText.hide();
+    return showTitleRequest();
+  };
+  onReady = function() {
+    console.log('ready');
+    $('#write-form').submit(onSubmit);
+    $('#writebox textarea').keypress(onkeypress);
+    requestTitle();
+    return titleTextBox().keypress(function() {
+      if (event.which === 13) {
+        return getTitle();
+      }
+    });
+  };
+  $(document).ready(onReady);
+}).call(this);
